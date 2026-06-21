759d9c9c6cde4853cb197e6284cef23ac76745ba
ADITYA OS Core Contributors <aditya.os.core@gmail.com>
2026-06-21 09:36:38 +0530

    chore: remove android build and capacitor configurations from client

--- client/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.aditya.dashboard',
-  appName: 'ADITYA',
-  webDir: 'dist'
-};
-
-export default config;
